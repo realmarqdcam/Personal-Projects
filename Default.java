3b3818dccf1ff069138510075c4f1eb9ee922c81
Marques Cameron <marqdcam@outlook.com>
2025-08-18 16:49:03 -0500

    Saving last progress

--- Default.java
+++ Default.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 public class Default {
+    //
     public static void main(String[] args) {
-        
+        Scanner plug = new Scanner(System.in);
+        HashMap<String, Double> grades = new HashMap<String, Double>();
+
     }
 }
